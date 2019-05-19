bdd4d9728434036cc81c201af9a5bc0379ecc85a
Max Stern <stern.maxwell@gmail.com>
2019-05-18 18:55:20 -0700

    server config

---
+++ server/config/config.js
@@ -0,0 +1,15 @@
+// DEV & PROD VARIABLES
+const config = {
+    production: {
+        SECRET: process.env.SECRET,
+        DATABASE: process.env.MONGODB_URI
+    },
+    default: {
+        SECRET: 'SUPERSECRETPASSWORD123',
+        DATABASE: 'mongodb://localhost:27017/bookReviews'
+    }
+}
+
+exports.get = function get(env) {
+    return config[env] || config.default
+}
---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const config = require('./config/config').get(process.env.NODE_ENV);
+
+const app = express();
+
+// MONGOOSE
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DATABASE);
+
+// MIDDLEWARE
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// PORT
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
